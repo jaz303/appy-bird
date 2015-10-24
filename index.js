e02035055ef7b8cfb40786371a969248a0525980
Jason Frame <jason@onehackoranother.com>
2015-10-24 12:59:14 +0100

    extract router to own file

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const querystring = require('querystring');
 
 const httpStatus = require('http-status');
 const mime = require('mime-types');
-const rescape = require('escape-string-regexp');
 const statik = require('node-static');
 
-const EMPTY = {};
-Object.freeze(EMPTY);
+const makeSimpleRouter = require('./lib/simple-router');
 
 const CORS_HEADERS = {
 	origin 		: 'Access-Control-Allow-Origin',
@@ -106,66 +104,6 @@ function sendResponse(res, status, headers, body) {
 
 function sendTextErrorResponse(res, status) {
 	sendResponse(res, status, {'Content-Type': 'text/plain'}, httpStatus[status] || 'Error');
-}
-
-function makeSimpleRouter(routes) {
-	
-	// preprocess routes
-	routes.forEach(function(r) {
-		
-		if (r.method) {
-			r.method = r.method.toUpperCase();
-		}
-
-		var path = r.path;
-		
-		if (typeof path === 'string') {
-			if (path.indexOf(':') >= 0) {
-				var names = [null];
-				path = new RegExp((rescape(path) + '$').replace(/\:(\w+)/g, function(_, name) {
-					names.push(name);
-					return '([^\/$]+)';
-				}));
-				r._matchPath = function(rp) {
-					var ms = rp.match(path);
-					if (!ms) return false;
-					var params = {};
-					for (var i = 1, l = names.length; i < l; ++i) {
-						params[names[i]] = ms[i];
-					}
-					return params;
-				}
-			} else {
-				r._matchPath = function(rp) {
-					return (path === rp) ? EMPTY : false;
-				}	
-			}
-		} else if (path instanceof RegExp) {
-			r._matchPath = function(rp) { return rp.match(path); }
-		} else {
-			r._matchPath = function(rp) { return EMPTY; }
-		}
-
-	});
-
-	function _matches(route, req) {
-		if (req.method !== 'OPTIONS') {
-			if (route.method && route.method !== req.method) {
-				return false;
-			}
-		}
-		return route._matchPath(req.uri.pathname);
-	}
-
-	return function(req) {
-		for (var i = 0; i < routes.length; ++i) {
-			var r = routes[i], m = _matches(r, req);
-			if (m) {
-				return [r, m];
-			}
-		}
-		return null;
-	}
 }
 
 function registerBodyHandler(contentType, read, handler) {
