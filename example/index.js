48e04f15d1739a396efe6654c29970bfa46e3947
Jason Frame <jason@onehackoranother.com>
2015-10-24 11:52:55 +0100

    example JSON API

--- example/index.js
+++ example/index.js
@@ -9,6 +9,12 @@ appy({
 		{
 			path: /^\/assets[\/$]/,
 			directory: __dirname + '/public'
+		},
+		{
+			path: '/test-api',
+			handler: function(req, matches, response) {
+				return response.json([1,2,3,4,5, Math.random(), matches]);
+			}
 		}
 	]
 }).listen(8080);
